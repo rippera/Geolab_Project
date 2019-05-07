7fc6885952ebc4d226f2ed82ffe6880009551903
rippera <43761181+rippera@users.noreply.github.com>
2019-05-07 19:12:28 +0400

    rippera

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ const burger = document.querySelector('.burger');
 const navWrapper = document.querySelector('.navigation__wrap')
 const navLiItems = document.querySelectorAll('.nav__li')
 const navLiItem = document.querySelector('.nav__li');
-
+const navigationWrapper = document.querySelector('.navigation__wrap');
+const goTopBtn = document.getElementById('goTop');
 // starting functions
 
 burger.addEventListener('click',()=>{
@@ -17,32 +18,40 @@ function headerAnimation() {
     if (topY > 200) {
         header.style.padding = '20px 0';
         header.style.boxShadow = '0px 4px 5px 0px rgba(0,0,0,0.75)';
-        navLiItem.style.fontSize = '18px';
+        // navLiItem.style.fontSize = '18px';
+        navigationWrapper.style.top = '73px';
+        goTopBtn.style.opacity = '1';
     }else{
         header.style.padding = '35px 0';
         header.style.boxShadow = '0px 4px 5px 0px rgba(0,0,0,0)';
-        navLiItem.style.fontSize = '20px';
+        // navLiItem.style.fontSize = '20px';
+        navigationWrapper.style.top = '103px';
+        goTopBtn.style.opacity = '0';
     }
 }
-let rame = document.querySelector('.nav__ul');
-(function test(){
-    let st = window.pageXOffset;
-    // if (st < 900) {
-    //     document.querySelector('.navigation__wrap').style.top = '73px';
-    //     console.log('shevida <900 ze');
-    // }else if (st < 500 + 'px'){
-    //     console.log('naklebi 500 ze ');
-    // }
-    switch (st < 900) {
-        case document.querySelector('.navigation__wrap').style.top = '73px':
-            break;
-        case st < 500 : 
-        rame.style.margin = '100px'
-        break;
-        default:
-            break;
-    }
-})()
+
+// let navUl = document.querySelector('.nav__ul');
+// (function test(e){
+//     // let windowXPos = window.pageXOffset;
+//     // let tests = 500;
+//     // switch (windowXPos) {
+        
+//     //     case windowXPos > tests:
+//     //     console.log('windowXPos');
+//     //     // document.querySelector('.navigation__wrap').style.top = '102px';
+//     //         break;
+//     //     case windowXPos > tests: 
+//     //      navUl.style.margin = '100px';
+//     //      console.log('900 ze naklebi ');
+//     //     break;
+//     //     default:
+//     //     // navUl.style.margin = '100px';
+//     //     break;
+//     // }
+    
+// })()
+
 window.addEventListener('scroll',()=>{
     headerAnimation();
 })
+
