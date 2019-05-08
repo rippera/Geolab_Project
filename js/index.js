ca4f1b88feeed9d076ae9ebd06a1395f6c113569
rippera <mikheil.mezurnishvili10b@geolab.edu.ge>
2019-05-08 15:51:55 +0400

    rippera

--- js/index.js
+++ js/index.js
@@ -5,53 +5,66 @@ const navLiItems = document.querySelectorAll('.nav__li')
 const navLiItem = document.querySelector('.nav__li');
 const navigationWrapper = document.querySelector('.navigation__wrap');
 const goTopBtn = document.getElementById('goTop');
+const navUl = document.querySelectorAll('.nav__ul')[0];
 // starting functions
+
+function addActiveClass(e){
+    let navActive = document.querySelector('.navli__active');
+    if (navActive != null) {
+        navActive.classList.remove('navli__active');
+    }
+    e.target.classList.add('navli__active');
+    burger.classList.remove('open');
+    navWrapper.classList.remove('nav_fixed');
+}
+
+function removeActiveClass(e){
+    let navActive = document.querySelector('.navli__active');
+    if (navActive != null) {
+        navActive.classList.remove('navli__active');
+    }
+    e.target.classList.remove('navli__active');
+    burger.classList.remove('open');
+    navWrapper.classList.remove('nav_fixed');
+}
+
+function mainAnimations() {
+    const header = document.querySelector('.header__');
+    const topY = window.pageYOffset;
+    if (topY > 200) {
+        header.style.padding = '20px 0';
+        header.style.boxShadow = '0px 4px 5px 0px rgba(0,0,0,0.75)';
+        navigationWrapper.style.top = '73px';
+        goTopBtn.style.opacity = '1';
+        goTopBtn.style.bottom = '0';
+    }else{
+        header.style.padding = '35px 0';
+        header.style.boxShadow = '0px 4px 5px 0px rgba(0,0,0,0)';
+        navigationWrapper.style.top = '103px';
+        goTopBtn.style.opacity = '0';
+        goTopBtn.style.bottom = '-30px';
+    }
+}
+
+// addEventListener functions
 
 burger.addEventListener('click',()=>{
     burger.classList.toggle('open');
     navWrapper.classList.toggle('nav_fixed');
 });
 
-function headerAnimation() {
-    const header = document.querySelector('.header__');
-    const topY = window.pageYOffset;
-    if (topY > 200) {
-        header.style.padding = '20px 0';
-        header.style.boxShadow = '0px 4px 5px 0px rgba(0,0,0,0.75)';
-        // navLiItem.style.fontSize = '18px';
-        navigationWrapper.style.top = '73px';
-        goTopBtn.style.opacity = '1';
-    }else{
-        header.style.padding = '35px 0';
-        header.style.boxShadow = '0px 4px 5px 0px rgba(0,0,0,0)';
-        // navLiItem.style.fontSize = '20px';
-        navigationWrapper.style.top = '103px';
-        goTopBtn.style.opacity = '0';
-    }
-}
-
-// let navUl = document.querySelector('.nav__ul');
-// (function test(e){
-//     // let windowXPos = window.pageXOffset;
-//     // let tests = 500;
-//     // switch (windowXPos) {
-        
-//     //     case windowXPos > tests:
-//     //     console.log('windowXPos');
-//     //     // document.querySelector('.navigation__wrap').style.top = '102px';
-//     //         break;
-//     //     case windowXPos > tests: 
-//     //      navUl.style.margin = '100px';
-//     //      console.log('900 ze naklebi ');
-//     //     break;
-//     //     default:
-//     //     // navUl.style.margin = '100px';
-//     //     break;
-//     // }
-    
-// })()
+navUl.addEventListener('click',addActiveClass);
 
 window.addEventListener('scroll',()=>{
-    headerAnimation();
-})
+    mainAnimations();
+});
 
+function responsiveFunction(x) {
+    if (x.matches) { 
+        navUl.addEventListener('click',removeActiveClass);
+        document.getElementById('parent').classList.remove('navli__active');
+    }
+}
+let x = window.matchMedia("(max-width: 992px)");
+responsiveFunction(x) ;
+x.addListener(responsiveFunction);
